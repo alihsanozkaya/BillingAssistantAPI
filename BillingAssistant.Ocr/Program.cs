9d81b038ed5ef98bf6ad99e1df5b5c4e7dde7e26
Ali İhsan <ihsanozkaya1903@gmail.com>
2024-01-16 01:09:40 +0300

    BillingAssistant.Ocr Class Libary'den Console App'e taşındı.

--- BillingAssistant.Ocr/Program.cs
+++ BillingAssistant.Ocr/Program.cs
@@ -1,33 +1,31 @@
 ﻿using IronOcr;
 
-class Program
+IronOcr.License.LicenseKey = "IRONSUITE.IHSANOZKAYA1903.GMAIL.COM.16554-55802DE2A5-BVSVHP5-TT6QALTILPTZ-43M4K2FMH2MC-EH4PXQEKJM5C-5I3O6LTEDY6C-2SVKVKNUWDS2-NU2JUPRSG6HD-HEPHWB-TYTYVA4DOMKLUA-DEPLOYMENT.TRIAL-SO3UTD.TRIAL.EXPIRES.05.FEB.2024";
+
+IronTesseract ocr = new();
+
+ocr.Language = OcrLanguage.English;
+ocr.AddSecondaryLanguage(OcrLanguage.Turkish);
+
+using (var input = new OcrInput(@"C:\Users\Ali\source\repos\BillingAssistant.Ocr\samsungFatura.pdf"))
 {
-    static void Main()
+    var Result = ocr.Read(input);
+    try
     {
-        IronOcr.License.LicenseKey = "IRONSUITE.IHSANOZKAYA1903.GMAIL.COM.16554-55802DE2A5-BVSVHP5-TT6QALTILPTZ-43M4K2FMH2MC-EH4PXQEKJM5C-5I3O6LTEDY6C-2SVKVKNUWDS2-NU2JUPRSG6HD-HEPHWB-TYTYVA4DOMKLUA-DEPLOYMENT.TRIAL-SO3UTD.TRIAL.EXPIRES.05.FEB.2024";
-        IronTesseract ocr = new();
-
-        ocr.Language = OcrLanguage.English;
-        ocr.AddSecondaryLanguage(OcrLanguage.Turkish);
-
-        using (var input = new OcrInput(@"C:\Users\Ali\source\repos\BillingAssistant.Ocr\samsungFatura.pdf"))
+        Result.SaveAsTextFile(@"C:\Users\Ali\source\repos\BillingAssistant.Ocr\Text.txt");
+        for (int i = 0; i < Result.Words.Length; i++)
         {
-            var Result = ocr.Read(input);
-            try
+            if (Result.Words[i].Text.ToLower() == "tutar")
             {
-                Result.SaveAsTextFile(@"C:\Users\Ali\source\repos\BillingAssistant.Ocr\Text.txt");
-                for (int i = 0; i < Result.Words.Length; i++)
-                {
-                    if (Result.Words[i].Text.ToLower() == "tutar")
-                    {
-                        Console.WriteLine(i + " Buldu");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred: {ex.Message}");
+                Console.WriteLine(i + " Buldu");
+
             }
         }
+        //Console.WriteLine(Result.Text);
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An error occured: {ex.Message}");
     }
 }
